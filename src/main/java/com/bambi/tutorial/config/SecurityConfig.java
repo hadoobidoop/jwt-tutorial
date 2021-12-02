9898b6ee927ec8504980d34dee7db64eff0f2767
hadoobidoop <hadoobidoop@gmail.com>
2021-12-02 16:37:50 +0900

    기본적인 security 설정을 구현하다.
    -test api에 대한 401 에러 해결

---
+++ src/main/java/com/bambi/tutorial/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.bambi.tutorial.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+/*
+@EnableWebSecurity 기본적인 web 보안을 활성화 하겠다는 의미의 어노테이션
+추가적인 설정을 위해 WebSecurityConfigurer를 implements하거나
+WebSecurityConfigurerAdaper를 extends 하는 방법이 있다!
+* */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근 제한을 설정하겠습니다
+        .antMatchers("/api/hello").permitAll() ///api/hello에 대한 요청은 인증없이 접근을 허용할게요
+        .anyRequest().authenticated(); //나머지 요청은 모두 인증이 필요해요
+  }
+}
